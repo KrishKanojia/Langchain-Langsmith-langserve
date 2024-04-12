1663e3e958f8e949ea16c9a52ec81386ca9214cf
Krish Kanojia <krishkanojia7@gmail.com>
2024-04-12 12:52:51 +0500

    first commit

---
+++ chatbot/app.py
@@ -0,0 +1,32 @@
+from langchain_community.llms import Ollama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser # Output Parser whenever LLM give any kind of response
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+
+# LangSmith Tracing
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
+
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question: {question}")
+    ]
+)
+
+# Streamlit Framework
+st.title('Langchain Demo With Ollama API')
+input_text = st.text_input("Search the topic u want")
+
+# Ollama LLM
+llm = Ollama(model="llama2")
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+if input_text:
+    st.write_stream(chain.stream({'question': input_text}))
